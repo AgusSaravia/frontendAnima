264ee8809611cbbe17301991d2ebf1451c353f76
Diego Carril <diegocarril48@gmail.com>
2025-05-29 21:48:08 -0300

    component card

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,67 @@
-import { ProductChooser } from "./components/swipe/Swipe";
-import Sidebar from "./components/Sidebar";
-
+// import { ProductChooser } from "./components/swipe/Swipe";
+// import Sidebar from "./components/Sidebar";
+import { Card } from "./components/Card";
 const products = [
-  { model: "S1000", name: "Mountain Bike", imageUrl: "/pexels-photo-8947698-3122970947.jpeg" },
-  { model: "R200", name: "Road Bike",     imageUrl: "/pexels-photo-8947698-3122970947.jpeg" },
-  { model: "C300", name: "City Cruiser",  imageUrl: "/pexels-photo-8947698-3122970947.jpeg" },
-  { model: "U400", name: "Urban Commuter",imageUrl: "/pexels-photo-8947698-3122970947.jpeg" },
+  {
+    model: "C1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, asperiores.",
+    imageUrl: "/src/assets/c1.jpg",
+    current: 15990,
+    timeLeft: "5h 23m",
+    bids: 5,
+  },
+  {
+    model: "C3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, asperiores.",
+
+    imageUrl: "/src/assets/c3.jpg",
+    current: 18990,
+    timeLeft: "4h 10m",
+    bids: 8,
+  },
+  {
+    model: "C4",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, asperiores.",
+
+    imageUrl: "/src/assets/c4.jpg",
+    current: 21990,
+    timeLeft: "3h 45m",
+    bids: 12,
+  },
+  {
+    model: "C5",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, asperiores.",
+
+    imageUrl: "/src/assets/c5.jpg",
+    current: 40990,
+    timeLeft: "2h 30m",
+    bids: 20,
+  },
+  {
+    model: "C3 Aircross",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, asperiores.",
+
+    imageUrl: "/src/assets/c3aircross.jpg",
+    current: 23990,
+    timeLeft: "2h 30m",
+    bids: 20,
+  },
 ];
-''
+
 function App() {
   return (
-    <>
-      <div className="flex justify-between">
-        <Sidebar />
-        <div className=" m-auto">
-          <ProductChooser products={products} />
-        </div>
+    <div className="w-full px-4 md:px-8 py-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-6">
+        {products.map((product, index) => (
+          <Card key={index} {...product} />
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,12 +1,7 @@
-
 export function Buttons() {
-    
-    return (
-        <div className="flex flex-col gap-5 justify-between w-6xl m-auto">
-        <button className="btn-theme-a">$100</button>
-        <button className="btn-theme-b">$200</button>
-        <button className="btn-theme-c">$300</button>
-        <button className="btn-theme-d">Custom</button>        
-        </div>
-    )
+  return (
+    <div className="flex flex-col gap-5 justify-between w-6xl m-auto">
+      <button className="">$100</button>
+    </div>
+  );
 }
