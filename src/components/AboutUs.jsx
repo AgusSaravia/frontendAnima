bce682b5c6b13afb3685e783d9d259dd8696f20b
Diego Carril <diegocarril48@gmail.com>
2025-06-05 19:41:06 -0300

    add InfoHome

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,10 +1,10 @@
 export function AboutUs() {
   return (
-    <div className="bg-[url('./assets/Montaña.png')] bg-cover bg-center bg-fixed h-screen">
+    <div className="bg-[url('./assets/Montañita_svg.svg')] bg-[50%_-180%] h-full bg-no-repeat h-screen bg-fixed">
       <div className="min-h-screen flex justify-center px-4 py-10">
         <div className="max-w-5xl w-full">
           <h1
-            className="text-white text-5xl font-extrabold text-center 
+            className="text-white mt-20 text-5xl font-extrabold text-center 
             [text-shadow:_-1px_-1px_0_#000,_1px_-1px_0_#000,_-1px_1px_0_#000,_1px_1px_0_#000] mb-8"
           >
             Sobre este Proyecto
---
+++ src/components/Info.jsx
@@ -0,0 +1,38 @@
+export function Info() {
+  return (
+    <div className="flex overflow-hidden mt-10">
+      <div className="w-1/2  flex items-center justify-center p-8">
+        <span className="text-[#485C11] font-extrabold text-5xl text-center">
+          Make your automatic offer and participate in the best opportunities
+          with a single click.
+        </span>
+      </div>
+
+      <div className="w-px bg-gray-900" />
+
+      <div className="w-1/2 p-8 space-y-6 ">
+        <div className="space-y-2">
+          <h2 className="text-2xl text-[#485C11] font-bold">Open Exhibition</h2>
+          <span className="block font-semibold">
+            The exhibition is open from the beginning until the end of the
+            online auction, the countdown will be public and in real time.
+          </span>
+        </div>
+
+        <div className="space-y-2">
+          <h2 className="text-2xl text-[#485C11] font-bold">Offer</h2>
+          <span className="block font-semibold">
+            With a few simple steps you can start bidding (See tutorial)
+          </span>
+        </div>
+
+        <div className="space-y-2">
+          <h2 className="text-2xl text-[#485C11] font-bold">Commissions</h2>
+          <span className="block font-semibold">
+            In the winning bid our commission will be 18.3% plus taxes
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
